3cc0bda9c00c9f0fbb71ca8c44bef1430769c89a
Lavlinsky-nikita <nikita.lavlinsky20022015@yandex.ru>
2023-01-21 15:48:32 +0300

    add config.py

---
+++ app/config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = 'e1c6fe5819c4bc8857e2cbce963a7be5904692eba862d1a809f195710aff5cdc'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:admin@10.0.2.15/devops_exam_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
